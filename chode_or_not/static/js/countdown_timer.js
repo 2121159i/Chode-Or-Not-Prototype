ae1569467955715604eba1b4294cf68beec1b4d5
Alex Irikov <airikov95@gmail.com>
2016-04-19 02:32:33 +0100

    Centered the clock vertically, now have to do so horizontally as well

--- chode_or_not/static/js/countdown_timer.js
+++ chode_or_not/static/js/countdown_timer.js
@@ -1,13 +1,22 @@
 /**
  * Created by 2121159i on 18/04/2016.
  */
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
 
 var clock = $('#countdown_timer').FlipClock(3600, { countdown: true });
+
 var myVar = setInterval(myTimer, 1000);
 
 function myTimer() {
     if (clock.getTime().time == 0){
-       setTimeout(function(){window.location.replace("/chodeOrNot");}, 1000);
+       setTimeout(function(){window.location.replace("/chodeOrNot/");}, 1000);
 
     }
 }
