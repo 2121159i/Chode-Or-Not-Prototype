fc5cbfd26c79205d50ff25a8cf945d2920a9b721
Alex Irikov <airikov95@gmail.com>
2016-05-23 04:06:53 +0300

    Removing the hour since it is not needed

--- chode_or_not/static/js/countdown_timer.js
+++ chode_or_not/static/js/countdown_timer.js
@@ -2,7 +2,10 @@
  * Created by 2121159i on 18/04/2016.
  */
 
-var clock = $('#countdown_timer').FlipClock(3600, { countdown: true });
+var clock = $('#countdown_timer').FlipClock(3599, {
+    countdown: true,
+    clockFace: 'MinuteCounter'
+});
 
 var myVar = setInterval(myTimer, 1000);
 
