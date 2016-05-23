0a3b62aea9b478ded311053496d0cced8794762d
Alex Irikov <airikov95@gmail.com>
2016-05-23 03:44:56 +0300

    Fixing the responsiveness of the flipclock manually using css (style.css), timer.html and countdown_timer.js

--- chode_or_not/static/js/countdown_timer.js
+++ chode_or_not/static/js/countdown_timer.js
@@ -1,14 +1,6 @@
 /**
  * Created by 2121159i on 18/04/2016.
  */
-jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
-                                                $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
-                                                $(window).scrollLeft()) + "px");
-    return this;
-}
 
 var clock = $('#countdown_timer').FlipClock(3600, { countdown: true });
 
