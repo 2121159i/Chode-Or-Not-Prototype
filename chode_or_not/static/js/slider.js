7cd25284fe94625855c3e5f8fccd19bc49e3f01e
Aleksandar Irikov <aleksandari@traveltek.net>
2016-06-22 14:03:28 +0100

    Removed the indicator below, finger sliding is enough

--- chode_or_not/static/js/slider.js
+++ chode_or_not/static/js/slider.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
     $('.collapsible').collapsible();
     $('.slider').slider({
-        full_width: true
+        full_width: true,
+        indicators: false,
     });
     $('.materialboxed').materialbox();
 });
